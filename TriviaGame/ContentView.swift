6a34219782c4e6461cf2b851537a07108bd8c644
Bananaslama277 <tracesmith555@gmail.com>
2023-03-13 08:30:28 -0500

    initial commit

--- TriviaGame/ContentView.swift
+++ TriviaGame/ContentView.swift
@@ -8,14 +8,106 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+
+            Color.red.ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                
+                Text("THE CLASSIC ROCK TRIVIA GAME")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Text("Click the START GAME button to begin")
+                    .font(.title)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        print("Button clicked")
+                    } label: {
+                        Text("")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .frame(width: 150.0)
+                            .fontWeight(.bold)
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("Button clicked")
+                    } label: {
+                        Text("")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .frame(width: 150.0)
+                            .fontWeight(.bold)
+                    }
+
+                    Spacer()
+
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        print("Button clicked")
+                    } label: {
+                        Text("")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .frame(width: 150.0)
+                            .fontWeight(.bold)
+                    }
+
+                    Spacer()
+                    
+                    Button {
+                        print("Button clicked")
+                    } label: {
+                        Text("")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .frame(width: 150.0)
+                            .fontWeight(.bold)
+                    }
+
+                    Spacer()
+
+                }
+
+                
+                Spacer()
+                
+                Button {
+                    print("Button clicked")
+                } label: {
+                    Text("START GAME")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                        .border(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/7/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                }
+            }
         }
-        .padding()
     }
 }
 
